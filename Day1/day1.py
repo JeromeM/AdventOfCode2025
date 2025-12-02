b3b5509fad1e59a4377628bf4b9d8bca27fc467b
Jérome Meyer <jerome@mantra.ms>
2025-12-02 10:45:44 +0100

    Day 1

---
+++ Day1/day1.py
@@ -0,0 +1,28 @@
+val = 50
+password1 = 0
+password2 = 0
+
+with open("input.txt") as f:
+    seq = tuple(line.strip() for line in f)
+
+# seq = ("L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82")
+
+for s in seq:
+
+    if s[0:1] == "L":
+        for i in range(1, int(s[1:]) + 1):
+            val = (100 if val == 0 else val)
+            password2 = (password2 + 1) if val == 100 else password2
+            val -= 1
+
+    if s[0:1] == "R":
+        for i in range(1, int(s[1:]) + 1):
+            val = (0 if val == 100 else val)
+            password2 = (password2 + 1) if val == 0 else password2
+            val += 1
+    
+    if val == 0 or val == 100:
+        password1 += 1
+
+print(f"Password 1: {password1}")
+print(f"Password 2: {password2}")
